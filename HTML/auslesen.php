3851ec83c3dd50e79adfc74ccce603e32261cd9c
r1vers0ng <lena.rothbauer@web.de>
2017-09-30 14:55:17 +0200

    php verändert

--- HTML/auslesen.php
+++ HTML/auslesen.php
@@ -6,6 +6,16 @@
         <form action="update.php" method="post">
             Frage:<br>
             <input type="text" name="question" placeholder="Wie geht es dir?"><br><br>
+            <select>
+            <?php
+                include("config.php");#
+                $sql = "SELECT * FROM `kategorien` ";
+                $erg = mysqli_query( $dbconnect, $sql );
+                while($row = mysqli_fetch_array($erg)) {
+                   echo "<option value='".$row['id']."'>".$row['name']."</option>";
+                }
+            ?>
+            </select>
             Kategorie:<br>
             <input type="text" name="category" placeholder="Begrüßung"><br><br>
             <input type="submit" value="Eintragen">
@@ -17,7 +27,7 @@
                 /*
                 Sucht Datenbankverbindungen aus config.php raus und aktualisiert die Datenbanken
                 */
-                include("config.php");
+                
                 $sql = "SELECT * FROM `fragen` ";
 	            $erg = mysqli_query( $dbconnect, $sql );
                 while($row = mysqli_fetch_array($erg)) {
